81f72987ff895c3ecb2c6012896b230093afe33d
Jason Magoto <magoto.14@buckeyemail.osu.edu>
2018-01-14 00:59:56 -0500

    Added archive download functionality to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Created 1/14/18
+
+require 'open-uri'
+
+# Variable to hold the url where the movie data is stored
+@url = 'https://datasets.imdbws.com/title.basics.tsv.gz'
+@archive_file_name = 'title.basics.tsv.gz'
+
+# Download the file, printing status messages before and after
+puts 'Downloading the movie database. This may take a few minutes.'
+File.write @archive_file_name, open(@url).read
+puts 'Finished downloading the movie database.'
